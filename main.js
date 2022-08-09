06adb1cba5525f5c791f1012f99a009d8263d763
eugeniamartino <108590342+eugeniamartino@users.noreply.github.com>
2022-08-09 15:08:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -50,9 +50,6 @@ const alumnos =[
    {id:3, nombreAlumno: "Pablo", notas: [9,6,9]},
 ]
 
-
-
-
 function sumarNota(notas){
    const filtroNotas = alumnos.filter((alumno) => alumnos.nota === notas);
    let numero = parseInt(prompt("Ingrese una nueva nota a"));
@@ -62,22 +59,27 @@ function sumarNota(notas){
 let cards = "";
 
 alumnos.forEach((alumno) => {
+   let sumatoria = alumno.notas.reduce((acumulador, elemento) => (acumulador + elemento) , 0)
+   let notasTotales = alumno.notas.length;
+   let promedio = (sumatoria / notasTotales);
+
    const idButton = `add-notas${alumno.id}`
    document.getElementById("section-card").innerHTML += `<div class='card'>
    <h2>${alumno.nombreAlumno}</h2>
-   <p> EL alumno tiene un promedio de: ${alumno.promedio} de un total de: ${alumno.cantidad} de notas</p>
+   <p> EL alumno tiene un promedio de: ${promedio.toFixed(2)} de un total de: ${notasTotales} de notas</p>
 
    <button id="${idButton}" onclick="sumarNota("notas")">Sumar Nota</button>
    </div>`; 
+
+})
+
+alumnos.forEach((alumno) => {
+   const idButton = `add-notas${alumno.id}`
+   document.getElementById(idButton).addEventListener("click", () => {
+      sumarNota(alumno.notas);
+   })
+   
 })
 
 
-   alumnos.forEach((alumno) => {
-      const idButton = `add-notas${alumno.id}`
-      document.getElementById(idButton).addEventListener("click", () => {
-         sumarNota();
-      })
-   })
-
-   
 //document.getElementById("section-card").innerHTML = cards; 
